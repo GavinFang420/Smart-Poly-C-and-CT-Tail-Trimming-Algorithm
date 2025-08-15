683fa60674e0e64a61b6f8bee9afaae659dba3ee
GavinFang420 <fangyihan060420@gmail.com>
2025-08-15 23:28:49 +0800

    Add fastp core files for FASTQ processing

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+/*
+ * SmartTailTrimmer - Intelligent polyC/CT tail trimming
+ * FASTQ I/O based on fastp (https://github.com/OpenGene/fastp)
+ * Original Copyright (c) 2017 OpenGene, MIT License
+ * Modified for SmartTailTrimmer
+ */
+
