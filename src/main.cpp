3189e2d515906819c2aef3da2371dd2c9262a977
Alisa Z. <139522014+AYJZ@users.noreply.github.com>
2025-08-17 17:41:06 +0800

    Delete src/main.cpp

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * SmartTailTrimmer - Intelligent polyC/CT tail trimming
- * FASTQ I/O based on fastp (https://github.com/OpenGene/fastp)
- * Original Copyright (c) 2017 OpenGene, MIT License
- * Modified for SmartTailTrimmer
- */
-
